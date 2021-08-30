f07c59c3857d3e4ce3043b000ceaba53b1be670d
Ridwan Ikhsan <ridwan@oneyel.com>
2021-08-30 17:54:10 +0700

    Update port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
